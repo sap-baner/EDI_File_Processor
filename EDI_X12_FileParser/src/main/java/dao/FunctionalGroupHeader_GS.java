a5852434362d09ac74bae71c2110c515d90e2424
Saptarshi Banerjee <34643189+sap-baner@users.noreply.github.com>
2017-12-18 16:19:38 +0530

    Add files via upload

---
+++ EDI_X12_FileParser/src/main/java/dao/FunctionalGroupHeader_GS.java
@@ -0,0 +1,100 @@
+package dao;
+
+public class FunctionalGroupHeader_GS {
+	
+	private String functIdentifierCode_GS01;
+	
+	private String appSenderCode_GS02;
+	
+	private String appReceiverCode_GS03;
+	
+	private String ediDate_GS04;
+	
+	private String ediTime_GS05;
+	
+	private String grpControlNo_GS06;
+	
+	private String respAgencyCode_GS07;
+	
+	private String ediVersionNo_GS08;
+	
+	
+	public String getFunctIdentifierCode_GS01() {
+		return functIdentifierCode_GS01;
+	}
+	
+	
+	public void setFunctIdentifierCode_GS01(String functIdentifierCode_GS01) {
+		this.functIdentifierCode_GS01 = functIdentifierCode_GS01;
+	}
+	
+	
+	public String getAppSenderCode_GS02() {
+		return appSenderCode_GS02;
+	}
+	
+	
+	public void setAppSenderCode_GS02(String appSenderCode_GS02) {
+		this.appSenderCode_GS02 = appSenderCode_GS02;
+	}
+	
+	
+	public String getAppReceiverCode_GS03() {
+		return appReceiverCode_GS03;
+	}
+	
+	
+	public void setAppReceiverCode_GS03(String appReceiverCode_GS03) {
+		this.appReceiverCode_GS03 = appReceiverCode_GS03;
+	}
+	
+	
+	public String getEdiDate_GS04() {
+		return ediDate_GS04;
+	}
+	public void setEdiDate_GS04(String ediDate_GS04) {
+		this.ediDate_GS04 = ediDate_GS04;
+	}
+	
+	
+	public String getEdiTime_GS05() {
+		return ediTime_GS05;
+	}
+	
+	
+	public void setEdiTime_GS05(String ediTime_GS05) {
+		this.ediTime_GS05 = ediTime_GS05;
+	}
+	
+	
+	public String getGrpControlNo_GS06() {
+		return grpControlNo_GS06;
+	}
+	
+	
+	public void setGrpControlNo_GS06(String grpControlNo_GS06) {
+		this.grpControlNo_GS06 = grpControlNo_GS06;
+	}
+	
+	
+	public String getRespAgencyCode_GS07() {
+		return respAgencyCode_GS07;
+	}
+	
+	
+	public void setRespAgencyCode_GS07(String respAgencyCode_GS07) {
+		this.respAgencyCode_GS07 = respAgencyCode_GS07;
+	}
+	
+	
+	public String getEdiVersionNo_GS08() {
+		return ediVersionNo_GS08;
+	}
+	
+	
+	public void setEdiVersionNo_GS08(String ediVersionNo_GS08) {
+		this.ediVersionNo_GS08 = ediVersionNo_GS08;
+	}
+	
+	
+}
