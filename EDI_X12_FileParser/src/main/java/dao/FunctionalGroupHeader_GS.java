16dce352bcd6b8e620f41af79308fb98e34418cd
Saptarshi Banerjee <34643189+sap-baner@users.noreply.github.com>
2017-12-18 16:15:54 +0530

    Added files for ISA, GS, ST and B10 segments

---
+++ EDI_X12_FileParser/src/main/java/dao/FunctionalGroupHeader_GS.java
@@ -0,0 +1,10 @@
+package dao;
+
+public class FunctionalGroupHeader_GS {
+	
+	private String functIdentifierCode_GS01;
+	private String appSenderCode_GS02;
+	private String appReceiverCode_GS03;
+	
+
+}
