51af883d6be9d2745d666ada733ce412e96061ea
Saptarshi <saptarshi.banerjee@citiustech.com>
2017-12-19 18:08:39 +0530

    SE Transaction Trailer pojo

---
+++ EDI_X12_FileParser/src/main/java/dao/SE_TransactionSetTrailer.java
@@ -0,0 +1,35 @@
+package dao;
+
+public class SE_TransactionSetTrailer {
+	
+	private String SE01_noOfIncludedSegments;
+	
+	private String SE01_transactionSetControlNo;
+	
+
+	
+	public String getSE01_noOfIncludedSegments() {
+		return SE01_noOfIncludedSegments;
+	}
+
+	
+	public void setSE01_noOfIncludedSegments(String sE01_noOfIncludedSegments) {
+		SE01_noOfIncludedSegments = sE01_noOfIncludedSegments;
+	}
+
+	
+	public String getSE01_transactionSetControlNo() {
+		return SE01_transactionSetControlNo;
+	}
+
+	
+	public void setSE01_transactionSetControlNo(String sE01_transactionSetControlNo) {
+		SE01_transactionSetControlNo = sE01_transactionSetControlNo;
+	}
+
+	
+	
+	
+	
+
+}
